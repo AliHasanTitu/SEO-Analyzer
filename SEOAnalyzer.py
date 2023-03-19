59c1d79e76245ba5cd6f78d897312870c7cef6e2
Titu <titu.socialnetmail@gmail.com>
2023-03-19 17:48:14 +0600

    upload the file in remote repo

---
+++ SEOAnalyzer.py
@@ -0,0 +1,8 @@
+from seoanalyzer import analyze
+
+# This is simple tool for seo
+site = "added a url, that are analyze you"
+site_map = "added sitemap of this website"
+
+output = analyze(site, site_map)
+print(output)
