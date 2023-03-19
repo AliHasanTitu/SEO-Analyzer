f817629e49297f24078fd6070459de4ee299d17e
Titu <titu.socialnetmail@gmail.com>
2023-03-19 17:51:24 +0600

    change the url in code

--- SEOAnalyzer.py
+++ SEOAnalyzer.py
@@ -1,8 +1,8 @@
 from seoanalyzer import analyze
 
 # This is simple tool for seo
-site = "added a url, that are analyze you"
-site_map = "added sitemap of this website"
+site = "https://abhinobo.com"
+site_map = "https://ahbinobo.com"
 
 output = analyze(site, site_map)
 print(output)
